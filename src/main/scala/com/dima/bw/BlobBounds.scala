56fb5b42425fbc675faf785c843bd9db809f9644
Your Name <you@example.com>
2016-10-24 13:10:26 -0400

    fixed memory reads counting issue

--- src/main/scala/com/dima/bw/BlobBounds.scala
+++ src/main/scala/com/dima/bw/BlobBounds.scala
@@ -8,7 +8,7 @@ object BlobBounds {
     val grid = GridGen.fromFile(file)
 
     val resultSlow = BlobFinder.naive(grid)
-    val resultFast = BlobFinder.heuristic(grid)
+    val resultFast = BlobFinder.smart(grid)
 
     println("Naive: " + resultSlow)
     println("Fast:  " + resultFast)
@@ -27,62 +27,44 @@ case class Result(boundary: Boundary, visited: Map[Coord, Boolean], path: List[C
 
 object BlobFinder {
   def naive(grid: Grid): Result = {
-    val b1: Boundary = new Boundary()
-
     val coords = {
       for {i <- 0 until grid.size; j <- 0 until grid.size}
         yield Coord(i, j)
     }.toList
 
-    Result(b1.updated(coords.filter(c => grid(c))), coords.map(c => (c, grid(c))).toMap, Nil)
+    Result(
+      new Boundary().updated(coords.filter(c => grid(c))),
+      coords.map(c => (c, grid(c))).toMap,
+      Nil)
   }
 
-  def breadth(grid: Grid): Result = {
-    val firstMoveOpt = grid.findFirst()
-
+  def smart(grid: Grid): Result = {
     def search(queue: List[Coord], visited: Map[Coord, Boolean], boundary: Boundary, path: List[Coord]): Result = {
       queue match {
         case Nil =>
-          Result(boundary, visited, path)
-        case head :: tail =>
-          val moves = grid.moves(head)
-          val validMoves = moves.filterNot(c => visited.contains(c))
-          val pathMoves = validMoves.filter(m => grid(m))
-          search(
-            tail ++ pathMoves,
-            visited ++ validMoves.map(m => (m, grid(m))),
-            boundary.updated(head),
-            head :: path)
-      }
-    }
-
-    firstMoveOpt.fold(Result(new Boundary(Coord(-1, -1)), Map.empty, Nil))(fm =>
-      search(List(fm.initial), fm.visited, fm.boundary, Nil))
-  }
-
-  def heuristic(grid: Grid): Result = {
-    val firstMoveOpt = grid.findFirst()
-
-    def search(queue: List[Coord], visited: Map[Coord, Boolean], boundary: Boundary, path: List[Coord]): Result = {
-      queue match {
-        case Nil =>
-          grid.boundaryMove(boundary, visited).fold(
-            Result(boundary, visited, path))(c => {
+          val nextMove = grid.boundaryMove(boundary, visited)
+          val newVisited = visited++nextMove._2.map(c=>(c,false))
+          if(nextMove._1==None)
+            Result(boundary, newVisited, path)
+          else {
+            val c = nextMove._1.get
             search(List(c), visited.updated(c, true), boundary.updated(c), path)
-          })
+          }
         case head :: tail =>
-          val moves = grid.moves(head)
-          val validMoves = moves.filterNot(c => visited.contains(c) || boundary.containsCoord(c))
-          val pathMoves = validMoves.filter(m => grid(m))
+          val validMoves = grid.moves(head)
+            .filterNot(c => visited.contains(c) || boundary.containsCoord(c))
+          val newVisited = visited ++ validMoves.map(m => (m, grid(m)))
+          val pathMoves = validMoves.filter(m => newVisited(m)) // avoid reading from the grid again
           search(
             tail ++ pathMoves,
-            visited ++ validMoves.map(m => (m, grid(m))),
+            newVisited,
             boundary.updated(pathMoves),
             head :: path)
       }
     }
 
-    firstMoveOpt.fold(Result(new Boundary(Coord(-1, -1)), Map.empty, Nil))(fm =>
+    grid.findFirst()
+      .fold(Result(new Boundary(Coord(-1, -1)), Map.empty, Nil))(fm =>
       search(List(fm.initial), fm.visited, fm.boundary, Nil))
   }
 }
@@ -126,17 +108,13 @@ case class Grid(size: Int) {
   val data: Array[Boolean] = new Array(size * size)
 
   def apply(coord: Coord) = data(coord.y * size + coord.x)
-
   def set(coord: Coord) = data(coord.y * size + coord.x) = true
 
-  def getCoord(n: Int) = Coord(n / size, n % size)
+  def coord(n: Int) = Coord(n / size, n % size)
 
   def left(p: Coord) = Coord(p.y, p.x - 1)
-
   def right(p: Coord) = Coord(p.y, p.x + 1)
-
   def down(p: Coord) = Coord(p.y + 1, p.x)
-
   def up(p: Coord) = Coord(p.y - 1, p.x)
 
   def legal(p: Coord): Boolean =
@@ -151,44 +129,37 @@ case class Grid(size: Int) {
     val points = scala.util.Random.shuffle((0 until size * size).toList)
     //val points = (0 until size*size).toList
 
-    val visited = points.takeWhile(p => !data(p)).map(n => getCoord(n)).map(coord => (coord, this (coord))).toMap
+    val visited = points.takeWhile(p => !data(p)).map(n => coord(n)).map(coord => (coord, this (coord))).toMap
 
     if (visited.size == size * size)
       None
     else
       Some(FirstMove(
-        getCoord(points(visited.size)),
+        coord(points(visited.size)),
         visited,
-        new Boundary(getCoord(points(visited.size)))
+        new Boundary(coord(points(visited.size)))
       ))
   }
 
   def moves(p: Coord): List[Coord] =
     List(left(p), right(p), up(p), down(p)).filter(legal)
 
-  def boundaryMove(boundary: Boundary, visited: Map[Coord, Boolean]): Option[Coord] = {
-    val stream = boundary.toStream.filterNot(visited.contains).filter(this (_))
+  def boundaryMove(boundary: Boundary, visited: Map[Coord, Boolean]): (Option[Coord],List[Coord]) = {
+    val stream = boundary.toStream.filterNot(visited.contains)
+    val empty = stream.takeWhile(!this(_)).toList
 
-    if (stream.nonEmpty)
-      Option(stream.head)
+    if(stream.nonEmpty)
+      (Option(stream.head),empty)
     else
-      None
+      (None,empty)
   }
 
   override def toString = {
-    val b = new StringBuilder()
+    def str(b: Boolean) = if(b) "1" else 0
 
-    for (i <- 0 until size) {
-      for (j <- 0 until size) {
-        if (this (Coord(i, j)))
-          b.append("1")
-        else
-          b.append("0")
-      }
-      b.append("\n")
-    }
-
-    b.toString
+    (0 until size).map(y=>{
+      (0 until size).map(x=>{str(this(Coord(y,x)))}).mkString
+    }).mkString("\n")
   }
 }
 
@@ -211,7 +182,7 @@ object GridGen {
   def random(size: Int): Grid = {
     val grid: Grid = Grid(size)
 
-    var current = grid.getCoord(Math.abs(scala.util.Random.nextInt()) % (size * size))
+    var current = grid.coord(Math.abs(scala.util.Random.nextInt()) % (size * size))
     val numMoves = Math.abs(scala.util.Random.nextInt()) % (size * size)
 
     for (i <- 0 until numMoves) {
